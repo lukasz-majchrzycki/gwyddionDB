36b18c3f450dbf794f8fa4068ba5c2b8f2c79f22
Łukasz Majchrzycki <lukasz.majchrzycki@gmail.com>
2019-11-13 15:03:42 +0100

    AfmImage data structure implemented

---
+++ src/main/java/pl/nanocode/gwyddionDB/AfmImage.java
@@ -0,0 +1,51 @@
+package pl.nanocode.gwyddionDB;
+
+import java.util.ArrayList;
+
+public class AfmImage {
+	public String title;
+	private double minZ, maxZ;		//z-scale min and max
+	private double xreal, yreal;	//horizontal and vertical dimensions in physical units
+	protected String si_unit_xy, si_unit_z;	//unit of lateral and z-scale dimensions
+	
+	protected ArrayList<Double> afmMap;		//z-scale data
+	private int xres, yres;	//horizontal and vertical size in pixels
+	
+	/*getters and setters*/
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getSi_unit_xy() {
+		return si_unit_xy;
+	}
+	public void setSi_unit_xy(String si_unit_xy) {
+		this.si_unit_xy = si_unit_xy;
+	}
+	public String getSi_unit_z() {
+		return si_unit_z;
+	}
+	public void setSi_unit_z(String si_unit_z) {
+		this.si_unit_z = si_unit_z;
+	}
+	public double getMinZ() {
+		return minZ;
+	}
+	public double getMaxZ() {
+		return maxZ;
+	}
+	public double getXreal() {
+		return xreal;
+	}
+	public double getYreal() {
+		return yreal;
+	}
+	public int getXres() {
+		return xres;
+	}
+	public int getYres() {
+		return yres;
+	}
+}
