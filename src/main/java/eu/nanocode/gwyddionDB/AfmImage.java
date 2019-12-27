2ab4153655fba8fbb10f087e5f6114db35dd9793
Łukasz Majchrzycki <lukasz.majchrzycki@gmail.com>
2019-12-27 13:42:22 +0100

    add imageID and description fields to AfmImage class and hibernate annotations

--- src/main/java/eu/nanocode/gwyddionDB/AfmImage.java
+++ src/main/java/eu/nanocode/gwyddionDB/AfmImage.java
@@ -1,16 +1,79 @@
 package eu.nanocode.gwyddionDB;
 
+import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
-public class AfmImage {	
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+ 
+@Entity
+@Table(name = "AfmImage", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "IMAGE_ID") })
+public class AfmImage implements Serializable {	
+	
+	private static final long serialVersionUID = -1798070786993154676L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name = "IMAGE_ID")
+	private long imageID;
+	
+	@Column(name = "TITLE", unique = false, nullable = true, length = 100)
 	public String title;
-	private double minZ, maxZ;		//z-scale min and max
-	private double xreal, yreal;	//horizontal and vertical dimensions in physical units
-	protected String si_unit_xy, si_unit_z;	//unit of lateral and z-scale dimensions
 	
-	protected ArrayList<Double> afmMap;		//z-scale data
-	private int xres, yres;	//horizontal and vertical size in pixels
+	//z-scale min and max
+	@Column(name = "MIN_Z", unique = false, nullable = false)
+	private double minZ;
 	
+	@Column(name = "MAX_Z", unique = false, nullable = false)
+	private double maxZ;		
+	
+	//horizontal and vertical dimensions in physical units	
+	@Column(name = "X_REAL", unique = false, nullable = false)
+	private double xreal;
+	
+	@Column(name = "Y_REAL", unique = false, nullable = false)
+	private double yreal;	
+	
+	//unit of lateral and z-scale dimensions
+	@Column(name = "SI_UNIT_XY", unique = false, nullable = true, length = 100)
+	protected String si_unit_xy;
+	
+	@Column(name = "SI_UNIT_Z", unique = false, nullable = true, length = 100)
+	protected String si_unit_z;	
+	
+	@Column(name = "CREATION_TIME", unique = false, nullable = false)
+	private LocalDateTime creationTime;
+	
+	@Column(name = "MODIFICATION_TIME", unique = false, nullable = false)
+	private LocalDateTime modificationTime;
+	
+	@Column(name = "DESCRIPTION", unique = false, nullable = true, length = 500)
+	public String description;
+	
+	//z-scale data
+	@Column(name = "AFM_MAP", unique = false, nullable = false, length = 4194304)
+	protected ArrayList<Double> afmMap;	
+	
+	//horizontal and vertical size in pixels
+	@Column(name = "X_RES", unique = false, nullable = false)
+	private int xres;	
+	
+	@Column(name = "Y_RES", unique = false, nullable = false)
+	private int yres;
+	
+	public AfmImage() {
+		this.creationTime = LocalDateTime.now();
+		this.modificationTime = LocalDateTime.now();
+		this.description="";
+	}
+
 	/*getters and setters*/
 	public String getTitle() {
 		return title;
@@ -66,4 +129,18 @@ public class AfmImage {
 	public void setYres(int yres) {
 		this.yres = yres;
 	}	
+	public long getImageID() {
+		return imageID;
+	}
+
+	public LocalDateTime getCreationTime() {
+		return creationTime;
+	}
+
+	public LocalDateTime getModificationTime() {
+		return modificationTime;
+	}
+	public void changeModificationTime() {
+		this.modificationTime = LocalDateTime.now();
+	}
 }
