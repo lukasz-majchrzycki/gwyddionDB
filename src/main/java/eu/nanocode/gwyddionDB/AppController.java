58c73c904bc001f2053d22d8bfa4c27f6a041caf
Łukasz Majchrzycki <lukasz.majchrzycki@gmail.com>
2020-01-02 20:22:42 +0100

    main structure of AppController

--- src/main/java/eu/nanocode/gwyddionDB/AppController.java
+++ src/main/java/eu/nanocode/gwyddionDB/AppController.java
@@ -1,5 +1,82 @@
 package eu.nanocode.gwyddionDB;
 
-public class AppController {
+import org.hibernate.Session;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+
+public class AppController {
+	
+	private boolean connState, leftPanState, rightPanState;
+	private long projID, imageID;
+	
+	private GwyddionDbConn conn;
+	private Session session;
+
+    @FXML
+    private Color x21;
+
+    @FXML
+    private Font x11;
+    
+    @FXML
+    private Font x3;
+
+    @FXML
+    private Color x4;
+   
+    @FXML
+    private Button connectButton;
+
+    @FXML
+    private Button openButton;
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private Button removeButton;
+
+    @FXML
+    private Button leftButton;
+
+    @FXML
+    private Button rightButton;
+
+    @FXML
+    private ImageView imgBox;
+
+    @FXML
+    private TableView<?> detailsTable;
+
+    @FXML
+    private TableView<?> projectList;
+
+    @FXML
+    private Label leftStatus;
+    
+    @FXML
+    private Label rightStatus;
+
+
+    public void connect() {
+    	}
+    	
+    }
+    
+    public void addImage() {
+    	
+    }
+
+    public void removeImage() {
+    	
+    }
+    
+    
 }
